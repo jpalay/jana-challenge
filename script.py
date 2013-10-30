0a1ea942c634e7d340bb0a9331c5c694b25917f6
Josh Palay <joshpalay@gmail.com>
2013-10-30 01:03:31 -0400

    Cleaning some things up

--- script.py
+++ script.py
@@ -5,22 +5,26 @@ import urllib2
 import xml.etree.ElementTree as ET
 from twilio.rest import TwilioRestClient
 
-from settings import XML_LOC, XML_URL, ACCOUNT_SID, AUTH_TOKEN, NUM
+import settings
 
-def edit_xml():
+def update_xml():
+    # reddit top story
     html = urllib2.urlopen('http://reddit.com/.json').read()
     d = json.loads(html)
     headline =  d['data']['children'][0]['data']['title']
 
-    x = ET.parse(XML_LOC)
+    x = ET.parse(settings.XML_LOC)
+
+    # Get all Say elements with id headline (xpath wouldn't work)
     say = [elt for elt in x.findall('.//Say') if elt.attrib.get('id', None) == 'headline']
     for elt in say:
         elt.text = headline
-    x.write(XML_LOC, encoding='UTF-8')
+    # edit xml file
+    x.write(settings.XML_LOC, encoding='UTF-8')
 
 def make_call():
-    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-    call = client.calls.create(url=XML_URL, to=NUM, from_='415-723-4236', IfMachine='Continue')
+    client = TwilioRestClient(settings.ACCOUNT_SID, settings.AUTH_TOKEN)
+    call = client.calls.create(url=settings.XML_URL, to=settings.NUM, from_='415-723-4236', IfMachine='Continue')
 
-edit_xml()
+update_xml()
 make_call()
